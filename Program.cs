f5bfe68fd45af8a6a7c32f6d319824f6cdfb22af
tiansentallane2006-cloud <tiansentallane@gmail.com>
2026-04-06 02:00:38 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL6_103022400100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusicTrack lagu = new SayaMusicTrack("Sample Track Title");
+            lagu.PrintTrackDetails();
+
+            lagu.IncreasePlayCount(5000);
+            lagu.PrintTrackDetails();
+
+            Console.WriteLine("\n--- Testing Overflow ---");
+            for (int i = 0; i < 220; i++)
+            {
+                lagu.IncreasePlayCount(10000000);
+            }
+
+            lagu.PrintTrackDetails();
+        }
+    }
+}
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+namespace TP_MODUL6_103022400100
+
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaMusicTrack(string title)
+        {
+            Debug.Assert(title != null);
+            Debug.Assert(title.Length <= 100);
+
+            this.title = title;
+            Random random = new Random();
+            this.id = random.Next(10000, 100000);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            Debug.Assert(count <= 10000000);
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("--- Track Details ---");
+            Console.WriteLine("ID         : " + id);
+            Console.WriteLine("Title      : " + title);
+            Console.WriteLine("Play Count : " + playCount);
+            Console.WriteLine("----------------------");
+        }
+    }
+}
